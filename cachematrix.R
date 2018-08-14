f195a2e1f82e3832f234550c6aae3f4837e29680
Pratyush <mohanty.pratyush@gail.com>
2018-08-14 14:41:01 +0100

    Updated comments for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,10 @@
 ## 2. get the value of the matrix
 ## 3. set the value of the inverse of the matrix
 ## 4. get the value of the inverse of the matrix
+## 
+## The function 'cacheSolve' returns the inverse from cache
+## if available. Else, stores the value to the cache and 
+## reuses it next time.
 
 ## Example:
 
